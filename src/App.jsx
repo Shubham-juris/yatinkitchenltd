c5b82d270c10bc2d98033f3a76c9f46d0721e3e8
It-juris <itjurisnexa@gmail.com>
2025-04-07 12:47:41 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 class='text-3xl font-bold underline text-center mt-20 py-20 bg-stone-600'>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
